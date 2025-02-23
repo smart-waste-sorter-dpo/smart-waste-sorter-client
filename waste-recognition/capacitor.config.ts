6e359ac6079b9352c3dabcd16cce3579ecfee9e5
Arina Topor <arinatopor@abanking.ru>
2025-02-23 13:04:10 +0500

    init project

---
+++ waste-recognition/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'waste-recognition',
+  webDir: 'dist/waste-recognition/browser',
+};
+
+export default config;
