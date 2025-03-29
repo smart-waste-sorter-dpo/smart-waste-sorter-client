9485a808c7172da87d9b32a642635795d0376a03
Arina Topor <arinatopor@abanking.ru>
2025-03-29 15:35:42 +0500

    delete proj

--- waste-recognition/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { OnboardingComponent } from './onboarding/onboarding.component';
-
-export const routes: Routes = [
-  { path: 'onboarding', component: OnboardingComponent },
-];
